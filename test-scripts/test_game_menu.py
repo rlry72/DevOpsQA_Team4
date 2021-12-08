23beafcce39a8d41bf7c8d9ac53e31ba5c0ef8ab
ryan <64831504+rlry72@users.noreply.github.com>
2021-12-08 14:32:09 +0800

    Create game menu and main menu exit test scripts (#2)
    * create game menu and main menu exit test scripts
    
    * added comments

---
+++ test-scripts/test_game_menu.py
@@ -0,0 +1,64 @@
+import pytest
+from tud_test_base import set_keyboard_input, get_display_output
+from classes.menu import *
+from classes.game import *
+
+
+def test_game_menu_display_board_options():
+    """
+    Tests if the game menu (game board and game options) are displayed properly upon starting a new game
+    Starts a new game and checks the game output against what the proper output should be.
+    """
+    # set input 0, but first input will be ignored.
+    set_keyboard_input(["0"])
+    # start new game and turn
+    test_game = Game()
+    test_game.start_new_turn()
+    # get what is printed in the console
+    result = get_display_output()
+    
+    # compares what is printed in console with what should be shown. if different, test fails.
+    assert result == ["Turn 1",
+    "    A     B     C     D  \n +-----+-----+-----+-----+\n1|     |     |     |     |\n +-----+-----+-----+-----+\n2|     |     |     |     |\n +-----+-----+-----+-----+\n3|     |     |     |     |\n +-----+-----+-----+-----+\n4|     |     |     |     |\n +-----+-----+-----+-----+",
+    "1. Build a SHP\n2. Build a SHP\n3. See remaining buildings\n4. See current score\n\n5. Save game\n0. Exit to main menu\n",
+    "Your choice? "]
+
+
+
+@pytest.mark.parametrize("invalidInput, expectedResult", 
+[("-1", ["Invalid input, please try again", "Your choice? "]), ("asdf", ["Invalid input, please try again", "Your choice? "]), 
+("13@!$`a", ["Invalid input, please try again", "Your choice? "]), (9, ["Invalid input, please try again", "Your choice? "]), ("", ["Invalid input, please try again", "Your choice? "])])
+def test_game_menu_invalid_input(invalidInput, expectedResult):
+    """
+    Runs a set of invalid inputs to test the input failure and message in the game menu. Tests to check if output messages are correct.
+    Invalid inputs are values outside of 0, 1, 2, 3, 4 and 5. 
+    Tested invalid inputs: special characters, integers, strings, out of bound number string.
+    """
+    # set input to start new game, then set invalid input and exit
+    set_keyboard_input(["1", "1", invalidInput, "0"])
+    # start game and new turn
+    testGame = Game()
+    testGame.start_new_turn()
+    # get the LAST TWO items printed in console. These last two items must be "Invalid input, please try again" and "Your choice? "
+    result = get_display_output()[-2:]
+    # compares console output to expected result set in pytest mark parametrize. if different, test fails
+    assert result == expectedResult
+
+
+def test_game_menu_return_main_menu():
+    """
+    Tests if the game can return to the main menu from the game menu. Tests to make sure the output in console is correct and main menu is displayed.
+    """
+    # set keyboard input to 0, then 0. (ignores 1)
+    set_keyboard_input(["1", "0", "0"])
+    # set expected output to that of main menu string
+    expectedOutput = ["Welcome, mayor of Simp City!        \n----------------------------\n1. Start new game\n2. Load saved game\n0. Exit\n",
+    "Your choice? "]
+    # starts a new game and new turn and enters game menu
+    selection = Game().start_new_turn()
+    # if input in game menu is 0,
+    if selection == 0:
+        # get output messages in console
+        result = get_display_output()
+        # compares output messages with the expected output set above. if different, test fails
+        assert result == expectedOutput
---
+++ test-scripts/test_main_menu_exit.py
@@ -0,0 +1,20 @@
+import pytest
+from tud_test_base import set_keyboard_input, get_display_output
+from classes.menu import *
+from classes.game import *
+
+
+def test_main_menu_exit():
+    """
+    Tests the exit function of main menu. This checks if game exits out of console or out of the game completely if opened from console properly.
+    """
+    # set keyboard input to 0 (ignores first 0)
+    set_keyboard_input(["0", "0"])
+    # starts main menu
+    main_menu()
+    # use pytest to raise a systemexit exception, then calls exit game function
+    with pytest.raises(SystemExit) as e:
+        exit_game()
+    # checks if exception type and value code are that of exit code. if different, test fails
+    assert e.type == SystemExit
+    assert e.value.code == 1
