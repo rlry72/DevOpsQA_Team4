2d008d3070ea20b09b0cb05da0ed56852f661106
PlatinumDL <ormiotar@gmail.com>
2021-12-08 14:07:28 +0800

    Added test script for build building and main menu

---
+++ test-scripts/testscriptQH/test_BuildBuilding_TestScriptsQH.py
@@ -0,0 +1,102 @@
+import pytest
+import os
+from classes.game import *
+from classes.menu import *
+from tud_test_base import set_keyboard_input, get_display_output
+from io import StringIO 
+import sys
+from unittest.mock import Mock
+
+
+
+
+def test_build_a_building():
+    #Test script to test Building a building from the Main Menu
+    set_keyboard_input(["1","a1","0"])
+
+    test_game = Game()
+    test_game.start_new_turn()
+    result = get_display_output()
+    
+    assert result == ["Turn 1",
+    "    A     B     C     D  \n +-----+-----+-----+-----+\n1|     |     |     |     |\n +-----+-----+-----+-----+\n2|     |     |     |     |\n +-----+-----+-----+-----+\n3|     |     |     |     |\n +-----+-----+-----+-----+\n4|     |     |     |     |\n +-----+-----+-----+-----+",
+    "1. Build a SHP\n2. Build a SHP\n3. See remaining buildings\n4. See current score\n\n5. Save game\n0. Exit to main menu\n",
+    "Your choice? ",
+    "Build where? ",
+    "Turn 2",
+    "    A     B     C     D  \n +-----+-----+-----+-----+\n1| SHP |     |     |     |\n +-----+-----+-----+-----+\n2|     |     |     |     |\n +-----+-----+-----+-----+\n3|     |     |     |     |\n +-----+-----+-----+-----+\n4|     |     |     |     |\n +-----+-----+-----+-----+",
+    "1. Build a SHP\n2. Build a SHP\n3. See remaining buildings\n4. See current score\n\n5. Save game\n0. Exit to main menu\n",
+    "Your choice? "]
+
+
+
+
+def test_build_a_building_invalid_input():
+    #Test Script to test Invalid input when building from game menu
+    set_keyboard_input(["1","2a","0"])
+    test_game = Game()
+    test_game.start_new_turn()
+    result = get_display_output()
+
+    assert result == ["Turn 1",
+    "    A     B     C     D  \n +-----+-----+-----+-----+\n1|     |     |     |     |\n +-----+-----+-----+-----+\n2|     |     |     |     |\n +-----+-----+-----+-----+\n3|     |     |     |     |\n +-----+-----+-----+-----+\n4|     |     |     |     |\n +-----+-----+-----+-----+",
+    "1. Build a SHP\n2. Build a SHP\n3. See remaining buildings\n4. See current score\n\n5. Save game\n0. Exit to main menu\n",
+    "Your choice? ",
+    "Build where? ",
+    "Your input is invalid, please follow 'letter' + 'digit' format to input for location.",
+    "Turn 1",
+    "    A     B     C     D  \n +-----+-----+-----+-----+\n1|     |     |     |     |\n +-----+-----+-----+-----+\n2|     |     |     |     |\n +-----+-----+-----+-----+\n3|     |     |     |     |\n +-----+-----+-----+-----+\n4|     |     |     |     |\n +-----+-----+-----+-----+",
+    "1. Build a SHP\n2. Build a SHP\n3. See remaining buildings\n4. See current score\n\n5. Save game\n0. Exit to main menu\n",
+    "Your choice? "]
+
+
+
+
+
+def test_build_a_building_invalid_location():
+    #Test Script to test Invalid location for building
+    set_keyboard_input(["1","a1","1","c4","0"])
+    test_game = Game()
+    test_game.start_new_turn()
+    result = get_display_output()
+
+    assert result == ["Turn 1",
+    "    A     B     C     D  \n +-----+-----+-----+-----+\n1|     |     |     |     |\n +-----+-----+-----+-----+\n2|     |     |     |     |\n +-----+-----+-----+-----+\n3|     |     |     |     |\n +-----+-----+-----+-----+\n4|     |     |     |     |\n +-----+-----+-----+-----+",
+    "1. Build a SHP\n2. Build a SHP\n3. See remaining buildings\n4. See current score\n\n5. Save game\n0. Exit to main menu\n",
+    "Your choice? ",
+    "Build where? ",
+    "Turn 2",
+    "    A     B     C     D  \n +-----+-----+-----+-----+\n1| SHP |     |     |     |\n +-----+-----+-----+-----+\n2|     |     |     |     |\n +-----+-----+-----+-----+\n3|     |     |     |     |\n +-----+-----+-----+-----+\n4|     |     |     |     |\n +-----+-----+-----+-----+",
+    "1. Build a SHP\n2. Build a SHP\n3. See remaining buildings\n4. See current score\n\n5. Save game\n0. Exit to main menu\n",
+    "Your choice? ",
+    "Build where? ",
+    "You must build next to an existing building.",
+    "Turn 2",
+    "    A     B     C     D  \n +-----+-----+-----+-----+\n1| SHP |     |     |     |\n +-----+-----+-----+-----+\n2|     |     |     |     |\n +-----+-----+-----+-----+\n3|     |     |     |     |\n +-----+-----+-----+-----+\n4|     |     |     |     |\n +-----+-----+-----+-----+",
+    "1. Build a SHP\n2. Build a SHP\n3. See remaining buildings\n4. See current score\n\n5. Save game\n0. Exit to main menu\n",
+    "Your choice? "]
+
+
+
+def test_build_a_building_build_on_existing_building():
+    #Test script to test building on existing building
+    set_keyboard_input(["1","a1","1","a1","0"])
+    test_game = Game()
+    test_game.start_new_turn()
+    result = get_display_output()
+
+    assert result == ["Turn 1",
+    "    A     B     C     D  \n +-----+-----+-----+-----+\n1|     |     |     |     |\n +-----+-----+-----+-----+\n2|     |     |     |     |\n +-----+-----+-----+-----+\n3|     |     |     |     |\n +-----+-----+-----+-----+\n4|     |     |     |     |\n +-----+-----+-----+-----+",
+    "1. Build a SHP\n2. Build a SHP\n3. See remaining buildings\n4. See current score\n\n5. Save game\n0. Exit to main menu\n",
+    "Your choice? ",
+    "Build where? ",
+    "Turn 2",
+    "    A     B     C     D  \n +-----+-----+-----+-----+\n1| SHP |     |     |     |\n +-----+-----+-----+-----+\n2|     |     |     |     |\n +-----+-----+-----+-----+\n3|     |     |     |     |\n +-----+-----+-----+-----+\n4|     |     |     |     |\n +-----+-----+-----+-----+",
+    "1. Build a SHP\n2. Build a SHP\n3. See remaining buildings\n4. See current score\n\n5. Save game\n0. Exit to main menu\n",
+    "Your choice? ",
+    "Build where? ",
+    "You cannot build on an existing building.",
+    "Turn 2",
+    "    A     B     C     D  \n +-----+-----+-----+-----+\n1| SHP |     |     |     |\n +-----+-----+-----+-----+\n2|     |     |     |     |\n +-----+-----+-----+-----+\n3|     |     |     |     |\n +-----+-----+-----+-----+\n4|     |     |     |     |\n +-----+-----+-----+-----+",
+    "1. Build a SHP\n2. Build a SHP\n3. See remaining buildings\n4. See current score\n\n5. Save game\n0. Exit to main menu\n",
+    "Your choice? "]
---
+++ test-scripts/testscriptQH/test_MainMenu_TestScripts.py
@@ -0,0 +1,57 @@
+import pytest
+import os
+from classes.game import *
+from classes.menu import *
+from tud_test_base import set_keyboard_input, get_display_output
+from io import StringIO 
+import sys
+from unittest.mock import Mock
+
+
+
+
+
+def test_main_menu_to_game_menu():
+    #Test script test going to main menu from game menu
+    set_keyboard_input(["1","0"])
+    selected = main_menu()
+
+    if (selected == "1"):
+        start_new_game()
+
+
+    result = get_display_output()
+    assert result == ["Welcome, mayor of Simp City!        \n----------------------------\n1. Start new game\n2. Load saved game\n0. Exit\n",
+    "Your choice? ",
+    "Turn 1",
+    "    A     B     C     D  \n +-----+-----+-----+-----+\n1|     |     |     |     |\n +-----+-----+-----+-----+\n2|     |     |     |     |\n +-----+-----+-----+-----+\n3|     |     |     |     |\n +-----+-----+-----+-----+\n4|     |     |     |     |\n +-----+-----+-----+-----+",
+    "1. Build a SHP\n2. Build a SHP\n3. See remaining buildings\n4. See current score\n\n5. Save game\n0. Exit to main menu\n",
+    "Your choice? "]
+
+
+
+
+
+
+def test_main_menu():
+    #Test script to test main menu output
+    set_keyboard_input(["0"])
+    main_menu()
+    result = get_display_output()
+
+    assert result == ["Welcome, mayor of Simp City!        \n----------------------------\n1. Start new game\n2. Load saved game\n0. Exit\n",
+    "Your choice? "]
+
+
+
+
+def test_main_menu_invalid_input():
+    #Test script to test invalid input in main menu
+    set_keyboard_input(["-1","0"])
+    main_menu()
+    result = get_display_output()
+
+    assert result == ["Welcome, mayor of Simp City!        \n----------------------------\n1. Start new game\n2. Load saved game\n0. Exit\n",
+    "Your choice? ",
+    "Invalid input, please try again",
+    "Your choice? "]
