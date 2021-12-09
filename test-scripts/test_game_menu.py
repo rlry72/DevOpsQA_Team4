461355bc5d8e323b2272ede8bcfe864ba9efa3e5
ryan <64831504+rlry72@users.noreply.github.com>
2021-12-09 12:01:21 +0800

    Fix game menu test scripts (#11)
    * fix test script game menu
    
    * made changes to error message
    
    Co-authored-by: PlatinumDL <64115176+PlatinumDL@users.noreply.github.com>

--- test-scripts/test_game_menu.py
+++ test-scripts/test_game_menu.py
@@ -63,8 +63,8 @@ def test_game_menu_display_board_options():
 
 
 @pytest.mark.parametrize("invalidInput, expectedResult", 
-[("-1", ['Please enter a valid input ("1" / "2" / "3" / "4" / "5" / "0").', "Your choice? "]), ("asdf", ['Please enter a valid input ("1" / "2" / "3" / "4" / "5" / "0").', "Your choice? "]), 
-("13@!$`a", ['Please enter a valid input ("1" / "2" / "3" / "4" / "5" / "0").', "Your choice? "]), (9, ['Please enter a valid input ("1" / "2" / "3" / "4" / "5" / "0").', "Your choice? "]), ("", ["Invalid input, please try again", "Your choice? "])])
+[("-1", ['Invalid Input. Please enter a valid input ("1" / "2" / "3" / "4" / "5" / "0").', "Your choice? "]), ("asdf", ['Invalid Input. Please enter a valid input ("1" / "2" / "3" / "4" / "5" / "0").', "Your choice? "]), 
+("13@!$`a", ['Invalid Input. Please enter a valid input ("1" / "2" / "3" / "4" / "5" / "0").', "Your choice? "]), (9, ['Invalid Input. Please enter a valid input ("1" / "2" / "3" / "4" / "5" / "0").', "Your choice? "]), ("", ['Invalid Input. Please enter a valid input ("1" / "2" / "3" / "4" / "5" / "0").', "Your choice? "])])
 def test_game_menu_invalid_input(invalidInput, expectedResult):
     """
     Runs a set of invalid inputs to test the input failure and message in the game menu. Tests to check if output messages are correct.
@@ -72,10 +72,9 @@ def test_game_menu_invalid_input(invalidInput, expectedResult):
     Tested invalid inputs: special characters, integers, strings, out of bound number string.
     """
     # set input to start new game, then set invalid input and exit
-    set_keyboard_input(["1", "1", invalidInput, "0"])
+    set_keyboard_input([invalidInput, "0"])
     # start game and new turn
-    testGame = Game()
-    testGame.start_new_turn()
+    testGame = Game().start_new_turn()
     # get the LAST TWO items printed in console. These last two items must be "Invalid input, please try again" and "Your choice? "
     result = get_display_output()[-2:]
     # compares console output to expected result set in pytest mark parametrize. if different, test fails
@@ -90,7 +89,7 @@ def test_game_menu_return_main_menu():
     # set keyboard input to 0, then 0. (ignores 1)
     set_keyboard_input(["1", "0", "0"])
     # set expected output to that of main menu string
-    expectedOutput = ["Welcome, mayor of Simp City!        \n----------------------------\n1. Start new game\n2. Load saved game\n0. Exit\n",
+    expectedOutput = ["Welcome, mayor of Simp City!\n----------------------------\n1. Start new game\n2. Load saved game\n\n0. Exit",
     "Your choice? "]
     # starts a new game and new turn and enters game menu
     selection = Game().start_new_turn()
