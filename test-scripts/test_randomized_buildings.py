048c5d58a83fc20d82b37bb50702a26f1d8a6070
ryan <64831504+rlry72@users.noreply.github.com>
2022-01-18 12:26:58 +0800

    Fixed test script and add new test case (#26)
    * add first test for randomized buildings
    
    * add test to compare outputs in 2 turns
    
    * add new test to check if options in building pool
    
    * fixed test script, add new test case
    
    * removed unnecessary tests
    
    * update descriptions and add comments

--- test-scripts/test_randomized_buildings.py
+++ test-scripts/test_randomized_buildings.py
@@ -1,10 +1,8 @@
-from distutils.command.build import build
 from classes.game import *
 from classes.menu import *
 from tud_test_base import set_keyboard_input, get_display_output
 from io import StringIO 
 from unittest.mock import Mock, patch
-from unittest import mock
 import sys
 import pytest
 import os
@@ -12,86 +10,32 @@ import random
 import math
 import statistics
 
-class SideEffect:
-    def __init__(self, *fns):
-        self.fs = iter(fns)
-    def __call__(self, *args, **kwargs):
-        f = next(self.fs)
-        return f(*args, **kwargs)
-
-def fake_randomized_buildings_1():
-    return ["BCH", "HSE"]
-
-def fake_randomized_buildings_2():
-    return ["SHP", "HWY"]
-
-
-def runsTest(l, l_median):
-  
-    runs, n1, n2 = 0, 0, 0
-      
-    # Checking for start of new run
-    for i in range(len(l)):
-          
-        # no. of runs
-        if (l[i] >= l_median and l[i-1] < l_median) or \
-                (l[i] < l_median and l[i-1] >= l_median):
-            runs += 1  
-          
-        # no. of positive values
-        if(l[i]) >= l_median:
-            n1 += 1   
-          
-        # no. of negative values
-        else:
-            n2 += 1   
-  
-    runs_exp = ((2*n1*n2)/(n1+n2))+1
-    stan_dev = math.sqrt((2*n1*n2*(2*n1*n2-n1-n2))/ \
-                       (((n1+n2)**2)*(n1+n2-1)))
-  
-    z = (runs-runs_exp)/stan_dev
-  
-    return z
-
-@mock.patch('classes.game.random_buildings', side_effect = SideEffect(fake_randomized_buildings_1, fake_randomized_buildings_2))
-def test_mock_randomized_building(mock_random_buildings):
+def test_compare_randomized_building_5_turns():
     """
-    Test script to test building a randomized building from the game menu
+    Test script to compare randomized building output over 5 turns
     """
-    set_keyboard_input(["1","a1","0"])
+    set_keyboard_input(["1","a1","1", "b1", "1", "c1", "1", "d1", "1", "a2","0"])
 
-    test_game = Game()
-    test_game.start_new_turn()
-    result = get_display_output()
-    
-    assert result == ["", "Turn 1",
-    "    A     B     C     D  \n +-----+-----+-----+-----+\n1|     |     |     |     |\n +-----+-----+-----+-----+\n2|     |     |     |     |\n +-----+-----+-----+-----+\n3|     |     |     |     |\n +-----+-----+-----+-----+\n4|     |     |     |     |\n +-----+-----+-----+-----+",
-    "1. Build a BCH\n2. Build a HSE\n3. See remaining buildings\n4. See current score\n\n5. Save game\n0. Exit to main menu",
-    "Your choice? ",
-    "Build where? ",
-    "",
-    "Turn 2",
-    "    A     B     C     D  \n +-----+-----+-----+-----+\n1| BCH |     |     |     |\n +-----+-----+-----+-----+\n2|     |     |     |     |\n +-----+-----+-----+-----+\n3|     |     |     |     |\n +-----+-----+-----+-----+\n4|     |     |     |     |\n +-----+-----+-----+-----+",
-    "1. Build a SHP\n2. Build a HWY\n3. See remaining buildings\n4. See current score\n\n5. Save game\n0. Exit to main menu",
-    "Your choice? "]
+    optList = []
 
-def test_compare_randomized_building_2_turns():
-    """
-    Test script to compare randomized building output over 2 turns
-    """
-    set_keyboard_input(["1","a1","0"])
-
+    # start game and input options and buildings
     test_game = Game()
     test_game.start_new_turn()
     result = get_display_output()
 
-    assert result[3] != result[9]
+    # get all options returned and add to list
+    for i in range(len(result)):
+        if "1" or "2" in result[i]:
+            optList.append(result[i])
+
+    # check that all are not similar
+    assert optList[0] != optList[1] != optList[2] != optList[3] != optList[4]
+    # assert result[3] != result[9] != result[15] != result[21] != result[26]
 
 @pytest.mark.skip(reason="no way of currently testing this, will manually test")
 def test_compare_randomized_building_10_turns():
     """
-    Test script to compare randomized building output over 10 turns
+    Test script to check randomized building output over 10 turns whether the randomizer is good enough
     """
 
     option1Arr = []
@@ -117,24 +61,57 @@ def test_check_randomized_building_in_building_pool():
     Test script to check that buildings in options are in building pool
     """
 
+    # empty option list
     optList = []
-    buildingPool = ["BCH", "FAC", "HSE", "SHP", "HWY"]
-
-
+   
     set_keyboard_input(["1","a1","1","a2","1","a3","1","a4","1","b1","1","b2","1","b3","1","b4","0"])
 
     test_game = Game()
+     # get building pool from game
+    buildingPool = test_game.building_pool
+    # start game
     test_game.start_new_turn()
     result = get_display_output()
 
+    # get all buildings returned in game menu in output
     for output in result:
         if "1." in output:
+            # split by \n and then by space and get the last item in list and append to options list
             opt1 = output.split("\n")[0].split(" ")[-1]
             optList.append(opt1)
         if "2." in output:
+            # split by \n and then by space and get the last item in list and append to options list
             opt2 = output.split("\n")[1].split(" ")[-1]
             optList.append(opt2)
+
+    # convert option list to set (no duplicates) and assert that option list is a subset of building pool.
     assert set(optList).issubset(buildingPool) is True
     # assert result[3] != result[9]
 
+def test_randomized_building_options_1_building_left():
+    """
+    Test script to test options when only 1 building is left
+    """
+    set_keyboard_input(["0"])
+
+    test_game = Game()
+    # set building pool to only have 1 SHP left and no other buildings
+    test_game.building_pool = {"HSE": 0, "FAC": 0, "SHP": 1, "HWY": 0, "BCH": 0}
+    test_game.start_new_turn()
+    result = get_display_output()
+    
+    # initialize options
+    opt1 = ""
+    opt2 = ""
+
+    for output in result:
+        if "1." in output:
+            # get options in output
+            opt1 = output.split("\n")[0].split(" ")[-1]
+        if "2." in output:
+            # get options in output
+            opt2 = output.split("\n")[1].split(" ")[-1]
+
+    # check that option 1 is the same as option 2
+    assert opt1 == opt2
 # need to test how random the randomized selections actually are
