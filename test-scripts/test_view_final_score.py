3cccc0a33f0b9f6cb32400f4a6ebe34423ad3ba4
PlatinumDL <ormiotar@gmail.com>
2022-01-25 17:55:00 +0800

    Fixed Identations

--- test-scripts/test_view_final_score.py
+++ test-scripts/test_view_final_score.py
@@ -23,265 +23,260 @@ gameMenu = ['1. Build a FAC',
             '0. Exit to main menu', 
             'Your choice? ']
 board1x1 = [
-    "     A  ",
-    "  +-----+",
-    " 1|     |",
-    "  +-----+",]
+"     A  ",
+"  +-----+",
+" 1|     |",
+"  +-----+",]
 
 board1x1Filled = [
-    "     A  ",
-    "  +-----+",
-    " 1| FAC |",
-    "  +-----+",]
+"     A  ",
+"  +-----+",
+" 1| FAC |",
+"  +-----+",]
 
 board2x2 = [
-    "     A     B  ",
-    "  +-----+-----+",
-    " 1|     | FAC |",
-    "  +-----+-----+",
-    " 2| FAC | FAC |",
-    "  +-----+-----+",]
+"     A     B  ",
+"  +-----+-----+",
+" 1|     | FAC |",
+"  +-----+-----+",
+" 2| FAC | FAC |",
+"  +-----+-----+",]
 
 board2x2Filled = [
-    "     A     B  ",
-    "  +-----+-----+",
-    " 1| FAC | FAC |",
-    "  +-----+-----+",
-    " 2| FAC | FAC |",
-    "  +-----+-----+",]
+"     A     B  ",
+"  +-----+-----+",
+" 1| FAC | FAC |",
+"  +-----+-----+",
+" 2| FAC | FAC |",
+"  +-----+-----+",]
 
 board3x3 = [
-    "     A     B     C  ",
-    "  +-----+-----+-----+",
-    " 1|     | FAC | FAC |",
-    "  +-----+-----+-----+",
-    " 2| FAC | FAC | FAC |",
-    "  +-----+-----+-----+",
-    " 3| FAC | FAC | FAC |",
-    "  +-----+-----+-----+",
+"     A     B     C  ",
+"  +-----+-----+-----+",
+" 1|     | FAC | FAC |",
+"  +-----+-----+-----+",
+" 2| FAC | FAC | FAC |",
+"  +-----+-----+-----+",
+" 3| FAC | FAC | FAC |",
+"  +-----+-----+-----+",
 ]
 
 board3x3Filled = [
-    "     A     B     C  ",
-    "  +-----+-----+-----+",
-    " 1| FAC | FAC | FAC |",
-    "  +-----+-----+-----+",
-    " 2| FAC | FAC | FAC |",
-    "  +-----+-----+-----+",
-    " 3| FAC | FAC | FAC |",
-    "  +-----+-----+-----+",
+"     A     B     C  ",
+"  +-----+-----+-----+",
+" 1| FAC | FAC | FAC |",
+"  +-----+-----+-----+",
+" 2| FAC | FAC | FAC |",
+"  +-----+-----+-----+",
+" 3| FAC | FAC | FAC |",
+"  +-----+-----+-----+",
 ]
 
 board4x4 = [
-    "     A     B     C     D  ",
-    "  +-----+-----+-----+-----+",
-    " 1|     | FAC | FAC | FAC |",
-    "  +-----+-----+-----+-----+",
-    " 2| FAC | FAC | FAC | FAC |",
-    "  +-----+-----+-----+-----+",
-    " 3| FAC | FAC | FAC | FAC |",
-    "  +-----+-----+-----+-----+",
-    " 4| FAC | FAC | FAC | FAC |",
-    "  +-----+-----+-----+-----+",]
+"     A     B     C     D  ",
+"  +-----+-----+-----+-----+",
+" 1|     | FAC | FAC | FAC |",
+"  +-----+-----+-----+-----+",
+" 2| FAC | FAC | FAC | FAC |",
+"  +-----+-----+-----+-----+",
+" 3| FAC | FAC | FAC | FAC |",
+"  +-----+-----+-----+-----+",
+" 4| FAC | FAC | FAC | FAC |",
+"  +-----+-----+-----+-----+",]
 
 board4x4Filled = [
-    "     A     B     C     D  ",
-    "  +-----+-----+-----+-----+",
-    " 1| FAC | FAC | FAC | FAC |",
-    "  +-----+-----+-----+-----+",
-    " 2| FAC | FAC | FAC | FAC |",
-    "  +-----+-----+-----+-----+",
-    " 3| FAC | FAC | FAC | FAC |",
-    "  +-----+-----+-----+-----+",
-    " 4| FAC | FAC | FAC | FAC |",
-    "  +-----+-----+-----+-----+",]
+"     A     B     C     D  ",
+"  +-----+-----+-----+-----+",
+" 1| FAC | FAC | FAC | FAC |",
+"  +-----+-----+-----+-----+",
+" 2| FAC | FAC | FAC | FAC |",
+"  +-----+-----+-----+-----+",
+" 3| FAC | FAC | FAC | FAC |",
+"  +-----+-----+-----+-----+",
+" 4| FAC | FAC | FAC | FAC |",
+"  +-----+-----+-----+-----+",]
 
 board5x5 = [
-            "     A     B     C     D     E  ",
-            "  +-----+-----+-----+-----+-----+",
-            " 1|     | FAC | FAC | FAC | FAC |",
-            "  +-----+-----+-----+-----+-----+",
-            " 2| FAC | FAC | FAC | FAC | FAC |",
-            "  +-----+-----+-----+-----+-----+",
-            " 3| FAC | FAC | FAC | FAC | FAC |",
-            "  +-----+-----+-----+-----+-----+",
-            " 4| FAC | FAC | FAC | FAC | FAC |",
-            "  +-----+-----+-----+-----+-----+",
-            " 5| FAC | FAC | FAC | FAC | FAC |",
-            "  +-----+-----+-----+-----+-----+",]
+"     A     B     C     D     E  ",
+"  +-----+-----+-----+-----+-----+",
+" 1|     | FAC | FAC | FAC | FAC |",
+"  +-----+-----+-----+-----+-----+",
+" 2| FAC | FAC | FAC | FAC | FAC |",
+"  +-----+-----+-----+-----+-----+",
+" 3| FAC | FAC | FAC | FAC | FAC |",
+"  +-----+-----+-----+-----+-----+",
+" 4| FAC | FAC | FAC | FAC | FAC |",
+"  +-----+-----+-----+-----+-----+",
+" 5| FAC | FAC | FAC | FAC | FAC |",
+"  +-----+-----+-----+-----+-----+",]
 
 board5x5Filled = [
-            "     A     B     C     D     E  ",
-            "  +-----+-----+-----+-----+-----+",
-            " 1| FAC | FAC | FAC | FAC | FAC |",
-            "  +-----+-----+-----+-----+-----+",
-            " 2| FAC | FAC | FAC | FAC | FAC |",
-            "  +-----+-----+-----+-----+-----+",
-            " 3| FAC | FAC | FAC | FAC | FAC |",
-            "  +-----+-----+-----+-----+-----+",
-            " 4| FAC | FAC | FAC | FAC | FAC |",
-            "  +-----+-----+-----+-----+-----+",
-            " 5| FAC | FAC | FAC | FAC | FAC |",
-            "  +-----+-----+-----+-----+-----+",]
+"     A     B     C     D     E  ",
+"  +-----+-----+-----+-----+-----+",
+" 1| FAC | FAC | FAC | FAC | FAC |",
+"  +-----+-----+-----+-----+-----+",
+" 2| FAC | FAC | FAC | FAC | FAC |",
+"  +-----+-----+-----+-----+-----+",
+" 3| FAC | FAC | FAC | FAC | FAC |",
+"  +-----+-----+-----+-----+-----+",
+" 4| FAC | FAC | FAC | FAC | FAC |",
+"  +-----+-----+-----+-----+-----+",
+" 5| FAC | FAC | FAC | FAC | FAC |",
+"  +-----+-----+-----+-----+-----+",]
 
 board6x6 = [
-            "     A     B     C     D     E     F  ",
-            "  +-----+-----+-----+-----+-----+-----+",
-            " 1|     | FAC | FAC | FAC | FAC | FAC |",
-            "  +-----+-----+-----+-----+-----+-----+",
-            " 2| FAC | FAC | FAC | FAC | FAC | FAC |",
-            "  +-----+-----+-----+-----+-----+-----+",
-            " 3| FAC | FAC | FAC | FAC | FAC | FAC |",
-            "  +-----+-----+-----+-----+-----+-----+",
-            " 4| FAC | FAC | FAC | FAC | FAC | FAC |",
-            "  +-----+-----+-----+-----+-----+-----+",
-            " 5| FAC | FAC | FAC | FAC | FAC | FAC |",
-            "  +-----+-----+-----+-----+-----+-----+",
-            " 6| FAC | FAC | FAC | FAC | FAC | FAC |",
-            "  +-----+-----+-----+-----+-----+-----+",]
+"     A     B     C     D     E     F  ",
+"  +-----+-----+-----+-----+-----+-----+",
+" 1|     | FAC | FAC | FAC | FAC | FAC |",
+"  +-----+-----+-----+-----+-----+-----+",
+" 2| FAC | FAC | FAC | FAC | FAC | FAC |",
+"  +-----+-----+-----+-----+-----+-----+",
+" 3| FAC | FAC | FAC | FAC | FAC | FAC |",
+"  +-----+-----+-----+-----+-----+-----+",
+" 4| FAC | FAC | FAC | FAC | FAC | FAC |",
+"  +-----+-----+-----+-----+-----+-----+",
+" 5| FAC | FAC | FAC | FAC | FAC | FAC |",
+"  +-----+-----+-----+-----+-----+-----+",
+" 6| FAC | FAC | FAC | FAC | FAC | FAC |",
+"  +-----+-----+-----+-----+-----+-----+",]
 
 board6x6Filled = [
-            "     A     B     C     D     E     F  ",
-            "  +-----+-----+-----+-----+-----+-----+",
-            " 1| FAC | FAC | FAC | FAC | FAC | FAC |",
-            "  +-----+-----+-----+-----+-----+-----+",
-            " 2| FAC | FAC | FAC | FAC | FAC | FAC |",
-            "  +-----+-----+-----+-----+-----+-----+",
-            " 3| FAC | FAC | FAC | FAC | FAC | FAC |",
-            "  +-----+-----+-----+-----+-----+-----+",
-            " 4| FAC | FAC | FAC | FAC | FAC | FAC |",
-            "  +-----+-----+-----+-----+-----+-----+",
-            " 5| FAC | FAC | FAC | FAC | FAC | FAC |",
-            "  +-----+-----+-----+-----+-----+-----+",
-            " 6| FAC | FAC | FAC | FAC | FAC | FAC |",
-            "  +-----+-----+-----+-----+-----+-----+",]
+"     A     B     C     D     E     F  ",
+"  +-----+-----+-----+-----+-----+-----+",
+" 1| FAC | FAC | FAC | FAC | FAC | FAC |",
+"  +-----+-----+-----+-----+-----+-----+",
+" 2| FAC | FAC | FAC | FAC | FAC | FAC |",
+"  +-----+-----+-----+-----+-----+-----+",
+" 3| FAC | FAC | FAC | FAC | FAC | FAC |",
+"  +-----+-----+-----+-----+-----+-----+",
+" 4| FAC | FAC | FAC | FAC | FAC | FAC |",
+"  +-----+-----+-----+-----+-----+-----+",
+" 5| FAC | FAC | FAC | FAC | FAC | FAC |",
+"  +-----+-----+-----+-----+-----+-----+",
+" 6| FAC | FAC | FAC | FAC | FAC | FAC |",
+"  +-----+-----+-----+-----+-----+-----+",]
+
+caseStudyBoardState = [
+"     A     B     C     D  ",
+"  +-----+-----+-----+-----+",
+" 1| SHP | SHP | HSE |     |",
+"  +-----+-----+-----+-----+",
+" 2| BCH | HSE | HSE | BCH |",
+"  +-----+-----+-----+-----+",
+" 3| BCH | SHP | HSE | HSE |",
+"  +-----+-----+-----+-----+",
+" 4| HWY | HWY | HWY | HWY |",
+"  +-----+-----+-----+-----+",]
+
+caseStudyBoardStateFilled = [
+"     A     B     C     D  ",
+"  +-----+-----+-----+-----+",
+" 1| SHP | SHP | HSE | FAC |",
+"  +-----+-----+-----+-----+",
+" 2| BCH | HSE | HSE | BCH |",
+"  +-----+-----+-----+-----+",
+" 3| BCH | SHP | HSE | HSE |",
+"  +-----+-----+-----+-----+",
+" 4| HWY | HWY | HWY | HWY |",
+"  +-----+-----+-----+-----+",]
     
 score1x1 = [
-    "HSE: 0",
-    "FAC: 1 = 1",
-    "SHP: 0",
-    "HWY: 0",
-    "BCH: 0",
-    "Total score: 1"]
+"HSE: 0",
+"FAC: 1 = 1",
+"SHP: 0",
+"HWY: 0",
+"BCH: 0",
+"Total score: 1"]
 
 score2x2 = [
-    "HSE: 0",
-    "FAC: 4 + 4 + 4 + 4 = 16",
-    "SHP: 0",
-    "HWY: 0",
-    "BCH: 0",
-    "Total score: 16"]
+"HSE: 0",
+"FAC: 4 + 4 + 4 + 4 = 16",
+"SHP: 0",
+"HWY: 0",
+"BCH: 0",
+"Total score: 16"]
 
 score3x3 = [
-    "HSE: 0",
-    "FAC: 4 + 4 + 4 + 4 + 1 + 1 + 1 + 1 + 1 = 21",
-    "SHP: 0",
-    "HWY: 0",
-    "BCH: 0",
-    "Total score: 21"]
+"HSE: 0",
+"FAC: 4 + 4 + 4 + 4 + 1 + 1 + 1 + 1 + 1 = 21",
+"SHP: 0",
+"HWY: 0",
+"BCH: 0",
+"Total score: 21"]
 
 score4x4 = [
-    "HSE: 0",
-    "FAC: 4 + 4 + 4 + 4 + 1 + 1 + 1 + 1 + 1 + 1 + 1 + 1 + 1 + 1 + 1 + 1 = 28",
-    "SHP: 0",
-    "HWY: 0",
-    "BCH: 0",
-    "Total score: 28"]
+"HSE: 0",
+"FAC: 4 + 4 + 4 + 4 + 1 + 1 + 1 + 1 + 1 + 1 + 1 + 1 + 1 + 1 + 1 + 1 = 28",
+"SHP: 0",
+"HWY: 0",
+"BCH: 0",
+"Total score: 28"]
 
 score5x5 = [
-    "HSE: 0",
-    "FAC: 4 + 4 + 4 + 4 + 1 + 1 + 1 + 1 + 1 + 1 + 1 + 1 + 1 + 1 + 1 + 1 + 1 + 1 + 1 + 1 + 1 + 1 + 1 + 1 + 1 = 37",
-    "SHP: 0",
-    "HWY: 0",
-    "BCH: 0",
-    "Total score: 37"]
+"HSE: 0",
+"FAC: 4 + 4 + 4 + 4 + 1 + 1 + 1 + 1 + 1 + 1 + 1 + 1 + 1 + 1 + 1 + 1 + 1 + 1 + 1 + 1 + 1 + 1 + 1 + 1 + 1 = 37",
+"SHP: 0",
+"HWY: 0",
+"BCH: 0",
+"Total score: 37"]
 
 score6x6 = [
-    "HSE: 0",
-    "FAC: 4 + 4 + 4 + 4 + 1 + 1 + 1 + 1 + 1 + 1 + 1 + 1 + 1 + 1 + 1 + 1 + 1 + 1 + 1 + 1 + 1 + 1 + 1 + 1 + 1 + 1 + 1 + 1 + 1 + 1 + 1 + 1 + 1 + 1 + 1 + 1 = 48",
-    "SHP: 0",
-    "HWY: 0",
-    "BCH: 0",
-    "Total score: 48"]
+"HSE: 0",
+"FAC: 4 + 4 + 4 + 4 + 1 + 1 + 1 + 1 + 1 + 1 + 1 + 1 + 1 + 1 + 1 + 1 + 1 + 1 + 1 + 1 + 1 + 1 + 1 + 1 + 1 + 1 + 1 + 1 + 1 + 1 + 1 + 1 + 1 + 1 + 1 + 1 = 48",
+"SHP: 0",
+"HWY: 0",
+"BCH: 0",
+"Total score: 48"]
+
+scoreCaseStudy = [
+"HSE: 1 + 5 + 5 + 3 + 3 = 17",
+"FAC: 1 = 1",
+"SHP: 2 + 2 + 3 = 7",
+"HWY: 4 + 4 + 4 + 4 = 16",
+"BCH: 3 + 3 + 3 = 9",
+"Total score: 50"]
 
 actualBoard4x4 = [
-                                 [Building(), Factory(1, 0), Factory(2, 0), Factory(3, 0)],
-                                 [Factory(0, 1), Factory(1, 1), Factory(2, 1), Factory(3, 1)],
-                                 [Factory(0, 2), Factory(1, 2), Factory(2, 2), Factory(3, 2)],
-                                 [Factory(0, 3), Factory(1, 3), Factory(2, 3), Factory(3, 3)]
-                             ]
+[Building(), Factory(1, 0), Factory(2, 0), Factory(3, 0)],
+[Factory(0, 1), Factory(1, 1), Factory(2, 1), Factory(3, 1)],
+[Factory(0, 2), Factory(1, 2), Factory(2, 2), Factory(3, 2)],
+[Factory(0, 3), Factory(1, 3), Factory(2, 3), Factory(3, 3)]]
 
 actualBoard1x1 = [
-    [Building()]
+[Building()]
 ]
 
 actualBoard2x2 = [
-    [Building(), Factory(1,0)],
-    [Factory(0,1), Factory(1,1)],
+[Building(), Factory(1,0)],
+[Factory(0,1), Factory(1,1)],
 ]
 
 actualBoard3x3 = [
-    [Building(), Factory(1,0), Factory(2,0)],
-    [Factory(0,1), Factory(1,1), Factory(2,1)],
-    [Factory(0,2), Factory(1,2), Factory(2,2)]
+[Building(), Factory(1,0), Factory(2,0)],
+[Factory(0,1), Factory(1,1), Factory(2,1)],
+[Factory(0,2), Factory(1,2), Factory(2,2)]
 ]
 
 actualBoard5x5 = [
-                                 [Building(), Factory(1, 0), Factory(2, 0), Factory(3, 0), Factory(4,0)],
-                                 [Factory(0, 1), Factory(1, 1), Factory(2, 1), Factory(3, 1), Factory(4, 1)],
-                                 [Factory(0, 2), Factory(1, 2), Factory(2, 2), Factory(3, 2), Factory(4, 2)],
-                                 [Factory(0, 3), Factory(1, 3), Factory(2, 3), Factory(3, 3), Factory(4, 3)],
-                                 [Factory(0, 4), Factory(1, 4), Factory(2, 4), Factory(3, 4), Factory(4, 4)],
-                             ]
+[Building(), Factory(1, 0), Factory(2, 0), Factory(3, 0), Factory(4,0)],
+[Factory(0, 1), Factory(1, 1), Factory(2, 1), Factory(3, 1), Factory(4, 1)],
+[Factory(0, 2), Factory(1, 2), Factory(2, 2), Factory(3, 2), Factory(4, 2)],
+[Factory(0, 3), Factory(1, 3), Factory(2, 3), Factory(3, 3), Factory(4, 3)],
+[Factory(0, 4), Factory(1, 4), Factory(2, 4), Factory(3, 4), Factory(4, 4)],]
 
 actualBoard6x6 = [
-                                 [Building(), Factory(1, 0), Factory(2, 0), Factory(3, 0), Factory(4,0), Factory(5, 0)],
-                                 [Factory(0, 1), Factory(1, 1), Factory(2, 1), Factory(3, 1), Factory(4, 1), Factory(5, 1)],
-                                 [Factory(0, 2), Factory(1, 2), Factory(2, 2), Factory(3, 2), Factory(4, 2), Factory(5, 2)],
-                                 [Factory(0, 3), Factory(1, 3), Factory(2, 3), Factory(3, 3), Factory(4, 3), Factory(5, 3)],
-                                 [Factory(0, 4), Factory(1, 4), Factory(2, 4), Factory(3, 4), Factory(4, 4), Factory(5, 4)],
-                                 [Factory(0, 5), Factory(1, 5), Factory(2, 5), Factory(3, 5), Factory(4, 5), Factory(5, 5)],
-                             ]
-
-caseStudyBoardState = [
-    "     A     B     C     D  ",
-    "  +-----+-----+-----+-----+",
-    " 1| SHP | SHP | HSE |     |",
-    "  +-----+-----+-----+-----+",
-    " 2| BCH | HSE | HSE | BCH |",
-    "  +-----+-----+-----+-----+",
-    " 3| BCH | SHP | HSE | HSE |",
-    "  +-----+-----+-----+-----+",
-    " 4| HWY | HWY | HWY | HWY |",
-    "  +-----+-----+-----+-----+",]
-
-caseStudyBoardStateFilled = [
-    "     A     B     C     D  ",
-    "  +-----+-----+-----+-----+",
-    " 1| SHP | SHP | HSE | FAC |",
-    "  +-----+-----+-----+-----+",
-    " 2| BCH | HSE | HSE | BCH |",
-    "  +-----+-----+-----+-----+",
-    " 3| BCH | SHP | HSE | HSE |",
-    "  +-----+-----+-----+-----+",
-    " 4| HWY | HWY | HWY | HWY |",
-    "  +-----+-----+-----+-----+",]
+[Building(), Factory(1, 0), Factory(2, 0), Factory(3, 0), Factory(4,0), Factory(5, 0)],
+[Factory(0, 1), Factory(1, 1), Factory(2, 1), Factory(3, 1), Factory(4, 1), Factory(5, 1)],
+[Factory(0, 2), Factory(1, 2), Factory(2, 2), Factory(3, 2), Factory(4, 2), Factory(5, 2)],
+[Factory(0, 3), Factory(1, 3), Factory(2, 3), Factory(3, 3), Factory(4, 3), Factory(5, 3)],
+[Factory(0, 4), Factory(1, 4), Factory(2, 4), Factory(3, 4), Factory(4, 4), Factory(5, 4)],
+[Factory(0, 5), Factory(1, 5), Factory(2, 5), Factory(3, 5), Factory(4, 5), Factory(5, 5)],]
 
 actualBoardCaseStudy = [
-                                 [Shop(0,0), Shop(1, 0), House(2, 0), Building()],
-                                 [Beach(0, 1), House(1, 1), House(2, 1), Beach(3, 1)],
-                                 [Beach(0, 2), Shop(1, 2), House(2, 2), House(3, 2)],
-                                 [Highway(0, 3), Highway(1, 3), Highway(2, 3), Highway(3, 3)]
-                             ]
-
-scoreCaseStudy = [
-    "HSE: 1 + 5 + 5 + 3 + 3 = 17",
-    "FAC: 1 = 1",
-    "SHP: 2 + 2 + 3 = 7",
-    "HWY: 4 + 4 + 4 + 4 = 16",
-    "BCH: 3 + 3 + 3 = 9",
-    "Total score: 50"]
-    
+[Shop(0,0), Shop(1, 0), House(2, 0), Building()],
+[Beach(0, 1), House(1, 1), House(2, 1), Beach(3, 1)],
+[Beach(0, 2), Shop(1, 2), House(2, 2), House(3, 2)],
+[Highway(0, 3), Highway(1, 3), Highway(2, 3), Highway(3, 3)]]
 
 
 @pytest.mark.parametrize("input, boardState, turnNumber, scoreBoard, heightWidth, actualBoard, boardStateFilled",
@@ -309,3 +304,4 @@ def test_view_final_score(input, boardState, turnNumber, scoreBoard, heightWidth
     result = get_display_output()
 
     assert result == [""] + turnNumberArray + boardState + gameMenu + ["Build where? ", "", "Final layout of Simp City:"] + boardStateFilled + scoreBoard+  mainMenuNoWelcome
+
