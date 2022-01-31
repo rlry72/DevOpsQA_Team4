4e9242bf3f5fc53720225de8db463c0bb77a5168
PlatinumDL <ormiotar@gmail.com>
2022-01-31 15:15:51 +0800

    Refactored View Remaining Buildings

--- test-scripts/test_see_remaining_buildings.py
+++ test-scripts/test_see_remaining_buildings.py
@@ -9,6 +9,44 @@ from classes.menu import *
 
 defaultBuildingPool = {"HSE":8, "FAC":8, "SHP": 8, "HWY":8, "BCH":8}
 
+gameMenu = ['1. Build a SHP',
+            '2. Build a SHP', 
+            '3. See remaining buildings',
+            '4. See current score',
+            '', 
+            '5. Save game', 
+            '0. Exit to main menu', 
+            'Your choice? ']
+
+board = [
+        "     A     B     C     D           Building   Remaining",
+        "  +-----+-----+-----+-----+        --------------------",
+        " 1|     |     |     |     |        HSE      | 8",
+        "  +-----+-----+-----+-----+        FAC      | 8",
+        " 2|     |     |     |     |        SHP      | 8",
+        "  +-----+-----+-----+-----+        HWY      | 8",
+        " 3|     |     |     |     |        BCH      | 8",
+        "  +-----+-----+-----+-----+",
+        " 4|     |     |     |     |",
+        "  +-----+-----+-----+-----+"]
+
+boardPlaced = [
+        "     A     B     C     D           Building   Remaining",
+        "  +-----+-----+-----+-----+        --------------------",
+        " 1| SHP |     |     |     |        HSE      | 8",
+        "  +-----+-----+-----+-----+        FAC      | 8",
+        " 2|     |     |     |     |        SHP      | 7",
+        "  +-----+-----+-----+-----+        HWY      | 8",
+        " 3|     |     |     |     |        BCH      | 8",
+        "  +-----+-----+-----+-----+",
+        " 4|     |     |     |     |",
+        "  +-----+-----+-----+-----+"]
+
+remainingBuildings = ["Building         Remaining\n--------         --------\nHSE              8\nFAC              8\nSHP              8\nHWY              8\nBCH              8\n"]
+
+remainingBuildingsPlaced = ["Building         Remaining\n--------         --------\nHSE              8\nFAC              8\nSHP              7\nHWY              8\nBCH              8\n"]
+
+
 def test_see_remaining_building_initial_state():
     """
     Test script to test see remaining buildings during turn 1 from the game menu
@@ -18,18 +56,11 @@ def test_see_remaining_building_initial_state():
 
     test_game = Game()
     test_game.building_pool = defaultBuildingPool
+    test_game.randomized_building_history = {"1": ["SHP", "SHP"], "2": ["SHP", "SHP"]}
     test_game.start_new_turn()
     result = get_display_output()
 
-    assert result == ["","Turn 1",
-    "    A     B     C     D  \n +-----+-----+-----+-----+\n1|     |     |     |     |\n +-----+-----+-----+-----+\n2|     |     |     |     |\n +-----+-----+-----+-----+\n3|     |     |     |     |\n +-----+-----+-----+-----+\n4|     |     |     |     |\n +-----+-----+-----+-----+",
-    "1. Build a SHP\n2. Build a SHP\n3. See remaining buildings\n4. See current score\n\n5. Save game\n0. Exit to main menu",
-    "Your choice? ",
-    "Building         Remaining\n--------         --------\nHSE              8\nFAC              8\nSHP              8\nHWY              8\nBCH              8\n",
-    "","Turn 1",
-    "    A     B     C     D  \n +-----+-----+-----+-----+\n1|     |     |     |     |\n +-----+-----+-----+-----+\n2|     |     |     |     |\n +-----+-----+-----+-----+\n3|     |     |     |     |\n +-----+-----+-----+-----+\n4|     |     |     |     |\n +-----+-----+-----+-----+",
-    "1. Build a SHP\n2. Build a SHP\n3. See remaining buildings\n4. See current score\n\n5. Save game\n0. Exit to main menu",
-    "Your choice? ",]
+    assert result == ["","Turn 1"] + board + gameMenu + remainingBuildings + ["", "Turn 1"] + board + gameMenu
  
    
 def test_see_remaining_building_after_play():
@@ -41,30 +72,12 @@ def test_see_remaining_building_after_play():
 
     test_game = Game()
     test_game.building_pool = defaultBuildingPool
+    test_game.randomized_building_history = {"1": ["SHP", "SHP"], "2": ["SHP", "SHP"]}
     test_game.start_new_turn()
     result = get_display_output()
 
-    assert result == ["","Turn 1",
-    "    A     B     C     D  \n +-----+-----+-----+-----+\n1|     |     |     |     |\n +-----+-----+-----+-----+\n2|     |     |     |     |\n +-----+-----+-----+-----+\n3|     |     |     |     |\n +-----+-----+-----+-----+\n4|     |     |     |     |\n +-----+-----+-----+-----+",
-    "1. Build a SHP\n2. Build a SHP\n3. See remaining buildings\n4. See current score\n\n5. Save game\n0. Exit to main menu",
-    "Your choice? ",
-    "Building         Remaining\n--------         --------\nHSE              8\nFAC              8\nSHP              8\nHWY              8\nBCH              8\n",
-    "","Turn 1",
-    "    A     B     C     D  \n +-----+-----+-----+-----+\n1|     |     |     |     |\n +-----+-----+-----+-----+\n2|     |     |     |     |\n +-----+-----+-----+-----+\n3|     |     |     |     |\n +-----+-----+-----+-----+\n4|     |     |     |     |\n +-----+-----+-----+-----+",
-    "1. Build a SHP\n2. Build a SHP\n3. See remaining buildings\n4. See current score\n\n5. Save game\n0. Exit to main menu",
-    "Your choice? ",
-    "Build where? ",
-    "",
-    "Turn 2",
-    "    A     B     C     D  \n +-----+-----+-----+-----+\n1| SHP |     |     |     |\n +-----+-----+-----+-----+\n2|     |     |     |     |\n +-----+-----+-----+-----+\n3|     |     |     |     |\n +-----+-----+-----+-----+\n4|     |     |     |     |\n +-----+-----+-----+-----+",
-    "1. Build a SHP\n2. Build a SHP\n3. See remaining buildings\n4. See current score\n\n5. Save game\n0. Exit to main menu",
-    "Your choice? ",
-    "Building         Remaining\n--------         --------\nHSE              8\nFAC              8\nSHP              7\nHWY              8\nBCH              8\n",
-    "",
-    "Turn 2",
-    "    A     B     C     D  \n +-----+-----+-----+-----+\n1| SHP |     |     |     |\n +-----+-----+-----+-----+\n2|     |     |     |     |\n +-----+-----+-----+-----+\n3|     |     |     |     |\n +-----+-----+-----+-----+\n4|     |     |     |     |\n +-----+-----+-----+-----+",
-    "1. Build a SHP\n2. Build a SHP\n3. See remaining buildings\n4. See current score\n\n5. Save game\n0. Exit to main menu",
-    "Your choice? ",
-    ]
+    assert result == ["","Turn 1"] + board + gameMenu + remainingBuildings + ["", "Turn 1"] + board + gameMenu + ["Build where? ","", "Turn 2"] + boardPlaced + gameMenu + remainingBuildingsPlaced + ["",
+    "Turn 2"] + boardPlaced + gameMenu
+
 
    
