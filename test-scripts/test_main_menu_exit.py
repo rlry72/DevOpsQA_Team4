2e9adf7c42ea9741622e1fccb07bed165bce570d
rlry72 <lee.ryan1703@gmail.com>
2022-02-02 22:58:09 +0800

    Refactor main menu exit script

--- test-scripts/test_main_menu_exit.py
+++ test-scripts/test_main_menu_exit.py
@@ -1,4 +1,5 @@
 import pytest
+from main import *
 from tud_test_base import set_keyboard_input, get_display_output
 from classes.menu import *
 from classes.game import *
@@ -11,10 +12,9 @@ def test_main_menu_exit():
     # set keyboard input to 0 (ignores first 0)
     set_keyboard_input(["0", "0"])
     # starts main menu
-    main_menu()
     # use pytest to raise a systemexit exception, then calls exit game function
     with pytest.raises(SystemExit) as e:
-        exit_game()
+        main()
     # checks if exception type and value code are that of exit code. if different, test fails
     assert e.type == SystemExit
     assert e.value.code == 1
