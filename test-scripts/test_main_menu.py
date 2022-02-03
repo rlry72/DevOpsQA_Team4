4d159af65423ede1eb1f0ddc578ef2eb69fba776
PlatinumDL <ormiotar@gmail.com>
2022-02-03 18:59:52 +0800

    Added indentation

--- test-scripts/test_main_menu.py
+++ test-scripts/test_main_menu.py
@@ -8,6 +8,7 @@ from io import StringIO
 import sys
 from unittest.mock import Mock
 import classes
+
 pytestmark = pytest.mark.skipif("generate_remaining_building_string" in dir(classes.game.Game), reason="view remaining buildings on side implemented")
 
 mainMenu = ["Welcome, mayor of Simp City!\n----------------------------\n1. Start new game\n2. Load saved game\n3. Show high scores\n4. Choose building pool\n5. Choose city size\n\n0. Exit",
